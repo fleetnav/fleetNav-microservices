f04dd5f7366422c17b68e4f4fc5e2d6a8a2a00b0
cvaz007 <sebastianmoreno301177@correo.itm.edu.co>
2024-05-02 08:24:34 -0500

    migrate the multi-tenant project to fleetNav and create a docker compose with mongo and postgress

--- fleetNav-multitenant-service/src/test/java/com/fleetNavmultitenantservice/FleetNavMultitenantServiceApplicationTests.java
+++ fleetNav-multitenant-service/src/test/java/com/fleetNavmultitenantservice/FleetNavMultitenantServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.fleetNavmultitenantservice;
+package com.fleetNav;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FleetNavMultitenantServiceApplicationTests {
+class fleetNavApplicationTests {
 
 	@Test
 	void contextLoads() {
