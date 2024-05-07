a8a66fb5f548ba1ed6fa59eed5a4cc7a3a0b27fb
Sebastian Moreno E <92068938+Cvaz007@users.noreply.github.com>
2024-05-07 09:46:41 -0500

    IT'S WORKING all gets petitions

--- fleetNav-service/src/main/java/com/fleetNav/infraestructure/mappers/TripMapper.java
+++ fleetNav-service/src/main/java/com/fleetNav/infraestructure/mappers/TripMapper.java
@@ -6,6 +6,7 @@ import com.fleetNav.domain.entities.Trip;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Mappings;
 
 import java.util.UUID;
 
@@ -14,6 +15,8 @@ public interface TripMapper {
     @Mapping(target = "id", ignore = true)
     Trip toTrip(TripRequest tripRequest);
 
+    @Mapping(target = "route.cost", source = "route.costId")
+    @Mapping(target = "route.stop", source = "route.stops")
     TripResponse toTripResponse(Trip trip);
 
     void updateFromTripRequest(TripRequest tripRequest, @MappingTarget Trip trip);
