98656bd2fa6760832c849c33f6f6fb95c6441c7e
cvaz007 <sebastianmoreno301177@correo.itm.edu.co>
2024-05-08 12:40:24 -0500

    change package

--- fleetNav-multitenant-service/src/main/java/com/fleetNavmultitenantservice/fleetNavApplication.java
+++ fleetNav-multitenant-service/src/main/java/com/fleetNavmultitenantservice/fleetNavApplication.java
@@ -1,4 +1,4 @@
-package com.fleetNavmultitenantservice.multitenantservice;
+package com.fleetNavmultitenantservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
