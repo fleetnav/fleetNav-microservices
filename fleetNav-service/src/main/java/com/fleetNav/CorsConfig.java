b7134f84b9190041f86a4761f7a75a442cd4caea
Sebastian Moreno E <92068938+Cvaz007@users.noreply.github.com>
2024-05-16 06:37:30 -0500

    add cors config feature

--- fleetNav-service/src/main/java/com/fleetNav/CorsConfig.java
+++ fleetNav-service/src/main/java/com/fleetNav/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.fleetNavchatservice;
+package com.fleetNav;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,11 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-        .allowedOrigins("*")
-        .allowedMethods("PUT", "DELETE", "POST", "GET", "PATCH");
-  }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("PUT", "DELETE", "POST", "GET", "PATCH");
+    }
 
 }
