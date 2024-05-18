205302de401727fb1108c75b84a9ff3de0968ca7
juanregino <juanreginopenagos@gmail.com>
2024-05-18 14:44:05 -0500

    add contact and relevant info in swagger documentation

--- fleetNav-service/src/main/java/com/fleetNav/service/config/swagger/SwaggerConfig.java
+++ fleetNav-service/src/main/java/com/fleetNav/service/config/swagger/SwaggerConfig.java
@@ -3,22 +3,38 @@ package com.fleetNav.service.config.swagger;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
         info = @Info(
-                title = "Documentation: fleetNav-Services Api",
-                version = "1.0",
-                description = "The fleetNav API documentation provides detailed information on the endpoints available to " +
-                        "interact with the fleet management platform. Use this API to integrate vehicle tracking, route " +
-                        "management and data analysis functionalities into your own application. The fleetNav API provides a " +
-                        "RESTful interface that allows you to access and manipulate fleet management related data efficiently " +
-                        "and securely."
-        ),
+        contact = @Contact(
+                            name = "Sebastian Moreno - Hector Montaña - Juan Pablo Regino", 
+                            url = "https://github.com/fleetnav/fleetNav-microservices", 
+                            email = "ralexale@gmail.com \n   juanreginopenagos@gmail.com \n sebastianmorenoecheverri@gmail.com"),
+        license = @License(name = "fleetnav", url = "https://github.com/fleetnav/fleetNav-microservices  LICENSE"),
+        title = "Documentation: fleetNav-Services Api", 
+        version = "1.0", 
+        description = "The fleetNav API documentation provides detailed information on the endpoints available to interact with the fleet management platform. Use this API to integrate vehicle tracking, route management and data analysis functionalities into your own application. The fleetNav API provides a RESTful interface that allows you to access and manipulate fleet management related data efficiently and securely."),
+        servers = { @Server (
+                            description = "Local ENV",  
+                            url = "http://localhost:8080"),
+                    @Server(
+                            description = "PROD ENV",  
+                            url = "http://Render:8080"),
+                  @Server(
+                            description = "Chat ENV",  
+                            url = "http://localhost:8083")
+                            
+        },
         security = {
                 @SecurityRequirement(
                         name = "bearerAuth"
